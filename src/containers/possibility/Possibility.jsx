abd7fa77a70a9e94e3b22091ddef9af3ed53e395
Gerhard <gerhard.putter@yahoo.com>
2021-12-21 18:44:31 +0200

    File structure.

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibility.css';
+
+const Posibility = () => {
+    return (
+        <div>
+            Posibility
+        </div>
+    )
+}
+
+export default Posibility
